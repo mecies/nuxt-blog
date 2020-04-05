bfa56807c9a6418e600cc01b736c6befe09885f4
mecies <maciekhnat@gmail.com>
2020-04-05 10:32:42 +0200

    add new post form content

--- pages/admin/newPost/index.vue
+++ pages/admin/newPost/index.vue
@@ -1,15 +1,48 @@
 <template>
   <div class="admin-post-page">
     <section class="new-post-form">
-      <form @click.prevent="createPost"></form>
+      <form @submit.prevent="onSave">
+        <AppControlInput v-model="editedPost.author"
+          >Author Name</AppControlInput
+        >
+        <AppControlInput v-model="editedPost.title">Title</AppControlInput>
+        <AppControlInput v-model="editedPost.thumbnailLiml"
+          >Thumbnail link</AppControlInput
+        >
+        <AppControlInput v-model="editedPost.content" control-type="textarea"
+          >Author Name</AppControlInput
+        >
+        <AppButton type="submit">Save</AppButton>
+        <AppButton @click="onCancel" btn-style="cancel" type="button"
+          >Cancel</AppButton
+        >
+      </form>
     </section>
   </div>
 </template>
 
 <script>
+import AppControlInput from "@/components/UI-Components/AppControlInput";
+import AppButton from "@/components/UI-Components/AppButton";
+
 export default {
+  data() {
+    return {
+      editedPost: {
+        author: "",
+        title: "",
+        thumbnailLiml: "",
+        content: "",
+      },
+    };
+  },
   methods: {
-    createPost() {},
+    onSave() {
+      // save the post
+    },
+    onCancel() {
+      //navigate back
+    },
   },
 };
 </script>
