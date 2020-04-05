1360568b83b5ee3b890ff0e18a8a9e35633aa2e1
mecies <maciekhnat@gmail.com>
2020-04-05 10:23:08 +0200

    Add reusable form components and admin route structure

---
+++ pages/admin/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="admin-page">
+    <section class="new-post">
+      <button>Create post</button>
+    </section>
+    <section class="existing-posts">
+      <h1>Existing posts</h1>
+      <PostList />
+    </section>
+  </div>
+</template>
+
+<script>
+import PostList from "@/components/Posts/PostList";
+export default {
+  components: {
+    PostList,
+  },
+};
+</script>
+
+<style scoped>
+.admin-page {
+  padding: 20px;
+}
+
+.new-post {
+  text-align: center;
+  border-bottom: 2px solid #cccccc;
+  padding-bottom: 10px;
+}
+
+.existing-posts h1 {
+  text-align: center;
+}
+</style>
---
+++ pages/admin/newPost/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="admin-post-page">
+    <section class="new-post-form">
+      <form @click.prevent="createPost"></form>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    createPost() {},
+  },
+};
+</script>
+
+<style></style>
