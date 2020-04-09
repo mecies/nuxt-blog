0f1e4a5eb0379b5285daded115c5996eb09f5594
mecies <maciekhnat@gmail.com>
2020-04-09 20:52:17 +0200

    date fix and add global date filter

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,7 +42,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: ["~plugins/globalComponents.js"],
+  plugins: ["~plugins/globalComponents.js", "~plugins/dateFilter.js"],
   /*
    ** Nuxt.js dev-modules
    */
